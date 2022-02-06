357f6c3f8a31ff5406cded6b23c19dc7e49ecf3e
missmahi55 <mahakagarwal248@gmail.com>
2022-02-06 23:40:48 +0530

    demo1 committed

---
+++ src/catalogue/sites/Components/D1Components/Navbar.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Navbar.css';
+
+function Navbar() {
+    return (
+        <div className="navbar">
+            <div className="logo">
+                <span>D</span>
+                <span>E</span>
+                <span>C</span>
+                <span>O</span>
+                <span style={{color:'red'}}>R</span>
+                <span>R</span>
+            </div>
+            <div className="D1navcontents">
+                <h5 style={{backgroundColor:'#e65c00',color:'white'}}>Home</h5>
+                <h5>Services</h5>
+                <h5>Our Work</h5>
+                <h5>Customer Reviews</h5>
+                <h5>Our Team</h5>
+                <h5>Contact</h5>
+                <h5 style={{color:'#e65c00'}}>Hire Us</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
---
+++ src/catalogue/sites/Demofourth.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Demofourth = () => {
+  return (
+    <div>
+      <h1>Demoforth</h1>
+    </div>
+  );
+};

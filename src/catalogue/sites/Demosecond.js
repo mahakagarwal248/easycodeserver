1cec5cc081523e5b8b738be48b99df8578331f46
missmahi55 <mahakagarwal248@gmail.com>
2022-02-12 10:45:50 +0530

    changes

--- src/catalogue/sites/Demosecond.js
+++ src/catalogue/sites/Demosecond.js
@@ -1,9 +1,12 @@
 import React from "react";
+import LoginForm1 from "./Components/D2Components/Loginform1/LoginForm1";
+import LoginForm2 from "./Components/D2Components/Loginform2/LoginForm2";
 
 export const Demosecond = () => {
   return (
     <div>
-      <h1>Demosecond</h1>
+      <LoginForm1/>
+      <LoginForm2/>
     </div>
   );
 };

ea99be8d118ced06d0c854a15394fe4f5a95faa1
missmahi55 <mahakagarwal248@gmail.com>
2022-02-24 21:04:47 +0530

    responsive navbar

--- client/src/catalogue/sites/Components/D2Components/section3/Section3.js
+++ client/src/catalogue/sites/Components/D2Components/section3/Section3.js
@@ -4,7 +4,7 @@ import LoginForm3 from './Loginform3/LoginForm3';
 
 function Section3() {
     return (
-        <div>
+        <div className="demo2s3">
             <section style={{display:'flex',backgroundColor: 'rgb(61, 7, 54)',color: 'white'}}>
                 <div className="demo2s3d1">Form 3</div>
                 <div className="demo2s3d2"><LoginForm3/></div>

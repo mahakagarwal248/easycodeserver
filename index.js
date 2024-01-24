3ecfb7a20119f35ce3f3a8ac9be9013bb73c50eb
Mahak Agarwal <mahakagarwal248@gmail.com>
2024-01-24 23:32:00 +0530

    fixes

--- index.js
+++ index.js
@@ -1,26 +1,17 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const path = require("path");
 const cors = require("cors");
-const fileUpload = require("express-fileupload");
-const app = express();
-app.use(express.json()); //this middleware used to get req.body
 
-dotenv.config({ path: "./config.env" });
+dotenv.config({ path: ".env" });
+const app = express();
 const port = process.env.PORT;
+app.use(express.json()); //this middleware used to get req.body
 app.use(cors());
-app.use(
-  fileUpload({
-    limits: { fileSize: 50 * 1024 * 1024 },
-  })
-);
-app.use("/contactus", require("./routers/contactus"));
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+app.get("/", (req, res) => {
+  res.send("Hello bhomik!");
+});
+app.use("/node/api/easycoder/contactus", require("./routers/contactus"));
+
 app.listen(port, () => {
   console.log(`server in runing :${port}`);
 });
